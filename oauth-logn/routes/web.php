3b2ad4a78f944b894a740ea09850ca05a03de090
19031240 <19031240@itcelaya.edu.mx>
2026-03-12 22:29:39 -0600

    Laravel Instalacion 1

--- oauth-logn/routes/web.php
+++ oauth-logn/routes/web.php
@@ -5,3 +5,13 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+use Laravel\Socialite\Facades\Socialite;
+
+Route::get('/login/discord', function () {
+    return Socialite::driver('discord')->redirect();
+});
+
+Route::get('/login/discord/callback', function () {
+    $user = Socialite::driver('discord')->user();
+    return $user;
+});
